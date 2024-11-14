324c1d4bff2e55b84c00ec9393a591c7f5a8ff9a
antof <fournier.anthony.jm@gmail.com>
2024-11-14 22:02:25 +0100

    Modification du contrat voting.sol
    Modification du calcul du winning proposal, afin de ne pas boucler sur l'array et eviter un Dos Gas Limit

--- backend/contracts/Voting.sol
+++ backend/contracts/Voting.sol
@@ -95,6 +95,10 @@ contract Voting is Ownable {
         voters[msg.sender].hasVoted = true;
         proposalsArray[_id].voteCount++;
 
+        //on calcule le winner temporaire
+        if(proposalsArray[_id].voteCount >proposalsArray[winningProposalID].voteCount)
+            winningProposalID = _id;
+
         emit Voted(msg.sender, _id);
     }
 
@@ -132,15 +136,7 @@ contract Voting is Ownable {
 
 
    function tallyVotes() external onlyOwner {
-       require(workflowStatus == WorkflowStatus.VotingSessionEnded, "Current status is not voting session ended");
-       uint _winningProposalId;
-      for (uint256 p = 0; p < proposalsArray.length; p++) {
-           if (proposalsArray[p].voteCount > proposalsArray[_winningProposalId].voteCount) {
-               _winningProposalId = p;
-          }
-       }
-       winningProposalID = _winningProposalId;
-       
+       require(workflowStatus == WorkflowStatus.VotingSessionEnded, "Current status is not voting session ended");       
        workflowStatus = WorkflowStatus.VotesTallied;
        emit WorkflowStatusChange(WorkflowStatus.VotingSessionEnded, WorkflowStatus.VotesTallied);
     }
