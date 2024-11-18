36246be16a918b3c77704a900cc52290f0e9e97b
antof <fournier.anthony.jm@gmail.com>
2024-11-18 09:20:21 +0100

    List of proposal
    Showing list of proposals.
    Added local viem client to fetch events

--- frontend/components/shared/Proposal.jsx
+++ frontend/components/shared/Proposal.jsx
@@ -3,16 +3,15 @@ import { useState, useEffect } from "react";
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-
+import { publicClient } from "@/utils/client"
 import { useToast } from "@/hooks/use-toast"
 
-import { useWriteContract, useWaitForTransactionReceipt, useAccount } from 'wagmi'
+import { useWriteContract, useWaitForTransactionReceipt, useAccount, useReadContract } from 'wagmi'
+import { parseAbiItem } from "viem"
 
 import { contractAddress, contractAbi } from "@/constants";
 
-import Informations from "./Informations";
-
-const Proposal = ({ refetch, getEvents }) => {
+const Proposal = () => {
 
     const { address } = useAccount();
     const { toast } = useToast()
@@ -20,6 +19,17 @@ const Proposal = ({ refetch, getEvents }) => {
 
     const { data: hash, isPending, error, writeContract } = useWriteContract()
 
+    const [proposals, setProposals] = useState([])
+
+    const getProposals = async() => {
+        const proposalsFetch = await publicClient.getLogs({
+          address: contractAddress,
+          event: parseAbiItem('event ProposalRegistered(uint proposalId, string description)'),
+          fromBlock: 0n,
+          toBlock: 'latest'
+        })
+        setProposals(proposalsFetch);
+    }
     const addProposal = async() => {
         if((proposalName).length !==0) {
             writeContract({
@@ -46,20 +56,35 @@ const Proposal = ({ refetch, getEvents }) => {
 
     useEffect(() => {
         if(isConfirmed) {
-            refetch()
             setProposalName('');
-            getEvents();
         }
+        getProposals();
     }, [isConfirmed])
+
+
 
     return (
         <>
-            <h2 className="text-4xl font-extrabold mb-4">Proposal</h2>
-            <Informations hash={hash} isConfirming={isConfirming} error={error} isConfirmed={isConfirmed} />
+            <h2 className="text-4xl font-extrabold mb-4">Add Proposal</h2>
             <div className="flex items-center">
                 <Input placeholder="Proposal Description" onChange={(e) => setProposalName(e.target.value)} value={proposalName} />
                 <Button onClick={addProposal} className="hover:bg-gray-600 bg-gray-700 text-white">Add Proposal</Button>
-                {console.log("value of proposal is : ", proposalName)}
+            </div>
+            {console.log(proposals)}
+            <h2 className="text-4xl font-extrabold mb-4">Proposals</h2>
+            <div className="flex items-center">
+
+            <div className="container">
+            <table className="table table-striped table-bordered">
+                <tbody>
+                    {proposals.map(proposal =>
+                        <tr key={proposal.args.proposalId}>
+                            <td>{proposal.args.description}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
             </div>
         </>
     )
